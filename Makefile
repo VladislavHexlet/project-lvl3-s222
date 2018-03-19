6e44e4f23827a066f0de068f97db02cdba7b1fd3
VladislavHexlet <vladislavprh1@gmail.com>
2018-03-19 16:58:18 +0300

    first step

--- Makefile
+++ Makefile
@@ -1,17 +1,29 @@
 install: install-deps install-flow-typed
 
+start:
+	npm start
+
+develop:
+	npm run webpack -- --watch --env development
+
 install-deps:
 	npm install
 
+install-flow-typed:
+	npm run flow-typed install
+
 build:
 	rm -rf dist
-	npm run build
+	npm run webpack -- -p --env production
 
 test:
 	npm test
 
+check-types:
+	npm run flow
+
 lint:
-	npm run eslint — src/
+	npm run eslint ./src
 
 publish:
 	npm publish
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log(2);
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const jumbotron = document.createElement('div');
+jumbotron.setAttribute('class', 'jumbotron');
+
+const rssForm = document.createElement('form');
+
+const textInput = document.createElement('input');
+textInput.setAttribute('type', 'text');
+const button = document.createElement('input');
+button.setAttribute('type', 'submit');
+
+[textInput, button].forEach(el => rssForm.append(el));
+jumbotron.append(rssForm);
+document.body.prepend(jumbotron);
